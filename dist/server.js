e153a73d500f0915cd8525d351892e5ebe63c04b
Your Name <you@example.com>
2023-01-22 22:37:04 +0100

    cors added

--- dist/server.js
+++ dist/server.js
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 import * as model from "./model.js";
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.status(200).send(model.getApiInstructions());
+});
+app.get("/test", (req, res) => {
+    const dateTime = new Date();
+    res.status(200).json({
+        message: `API test accessed at ${dateTime.toTimeString()}`,
+    });
 });
 app.listen(port, () => {
     console.log(`server listining on http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import * as model from "./model.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send(model.getApiInstructions());
+});
+
+app.get("/test", (req: express.Request, res: express.Response) => {
+  const dateTime = new Date();
+  res.status(200).json({
+    message: `API test accessed at ${dateTime.toTimeString()}`,
+  });
 });
 
 app.listen(port, () => {
